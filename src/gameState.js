7a9e8c94bad26d3883eec4febb8ef394db5cc8f4
sksaifuddin <atushaik@gmail.com>
2021-04-06 20:08:48 +0530

    completed UI interaction

---
+++ src/gameState.js
@@ -0,0 +1,14 @@
+const gameState = {
+    current: "INIT",
+    clock: 1,
+    tick() {
+        this.clock++;
+        console.log('clocl', this.clock);
+        return this.clock;
+    },
+    handleUserAction(icon) {
+        console.log(icon);
+    }
+};
+
+export default gameState;
